2db488ce851707e2aec55dd8f40562ec5910f536
Rapca Catalin <rapceacatalinc2gmail.com>
2024-03-29 12:32:38 +0200

    user class

---
+++ src/model/User.java
@@ -0,0 +1,67 @@
+package model;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String email;
+    private String password;
+    private float balance;
+
+    public User(int userId, String userName, String email, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+}
